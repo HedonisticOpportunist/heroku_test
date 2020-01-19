a53dc3604b60dc1e0105f1714a54df96ac603c04
HedonisticOpportunist <fujitahibaraki@gmail.com>
2020-01-19 17:01:58 +0000

    refining env file

--- heroku_app/features/support/env.rb
+++ heroku_app/features/support/env.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 require 'capybara/dsl'
 require 'capybara/cucumber'
 require 'capybara/rspec'
@@ -13,11 +12,10 @@ FileUtils.mkdir_p('reports/json') unless File.exist?('reports/json')
 def register_driver
   Capybara.register_driver :firefox do |app|
     options = Selenium::WebDriver::Firefox::Options.new
-    options.headless! if ENV['HEADLESS']
-    # options = Selenium::WebDriver::Firefox::Options.new
     Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
   end
   Capybara.default_driver = :firefox
   Capybara.javascript_driver = :firefox
   Capybara.app_host = 'http://computer-database.herokuapp.com/computers'
 end
+
